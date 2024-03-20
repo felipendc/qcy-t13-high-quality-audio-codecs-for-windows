194e58c759fc816849a07a38478b8587bd3578eb
vicyoslife <felipendc10phoenix@gmail.com>
2024-03-20 20:04:30 -0300

    cmd window shut down error fixed

--- TESTING_CODES/new/install_codecs - Copia.bat
+++ TESTING_CODES/new/install_codecs - Copia.bat
@@ -6,15 +6,15 @@ cd /d "%~dp0"
 
 @REM Grant permission and take ownership of some folders:
 takeown /f "C:\Windows\System32" /r /d y
-icacls "C:\Windows\System32" /grant "%USERNAME%":(OI)(CI)F
+icacls "C:\Windows\System32" /grant "%USERNAME%":"(OI)(CI)F"
 
 
 
 @REM Grant permission and take ownership of codec related folders:
 takeown /f "C:\Windows\System32\migwiz" /r /d y
-icacls "C:\Windows\System32\migwiz" /grant "%USERNAME%":(OI)(CI)F
+icacls C:\Windows\System32\migwiz /grant "%USERNAME%":"(OI)(CI)F"
 takeown /f "C:\Windows\System32\migwiz\replacementmanifests" /r /d y
-icacls "C:\Windows\System32\migwiz\replacementmanifests" /grant "%USERNAME%":(OI)(CI)F
+icacls C:\Windows\System32\migwiz\replacementmanifests /grant "%USERNAME%":"(OI)(CI)F"
 
 
 
@@ -22,14 +22,14 @@ icacls "C:\Windows\System32\migwiz\replacementmanifests" /grant "%USERNAME%":(OI
 for /f "tokens=*" %%i in (files\codecs_to_be_installed.txt) do (
     IF EXIST "C:\Windows\System32\%%i" (
         takeown "/f" "C:\Windows\System32\%%i"
-        icacls "C:\Windows\System32\%%i" "/grant" "%USERNAME%":(OI)(CI)F
+        icacls C:\Windows\System32\%%i "/grant" "%USERNAME%":"(OI)(CI)F"
     )
 )
 
 for /f "tokens=*" %%i in (files\codecs_to_be_removed.txt) do (
     IF EXIST "C:\Windows\System32\%%i" (
         takeown /f "C:\Windows\System32\%%i"
-        icacls "C:\Windows\System32\%%i" /grant "%USERNAME%":(OI)(CI)F 
+        icacls C:\Windows\System32\%%i /grant "%USERNAME%":"(OI)(CI)F" 
     )
 )
 
