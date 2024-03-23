3bad105502895210903a56bd891f12192cba9ec2
vicyoslife <felipendc10phoenix@gmail.com>
2024-03-23 17:43:15 -0300

    Finding and renaming mixer* files.

--- TESTING_CODES/AUTOMATED_INSTALLATION/1_RUN_AS_ADMIN_install_codecs.bat
+++ TESTING_CODES/AUTOMATED_INSTALLATION/1_RUN_AS_ADMIN_install_codecs.bat
@@ -4,6 +4,14 @@
 @echo off
 cd /d "%~dp0"
 
+@REM Save the root of this directory.
+set root_folder=%cd%
+
+@REM Find files and folder containing *mixer* in their names and save its paths to a .txt file.
+cd C:\Windows\System32 && dir /s /b "*mixer*" /b >> "%root_folder%\files\mixer_files_and_folders_directories.txt"
+cd %root_folder%
+
+
 
 
 @REM Grant permission and take ownership of some folders:
@@ -24,15 +32,25 @@ icacls C:\Windows\System32\migwiz\replacementmanifests /grant "%USERNAME%":"(OI)
 @REM Grant permission and take ownership of codec related files:
 for /f "tokens=*" %%i in (files\codecs_to_be_installed.txt) do (
     IF EXIST "C:\Windows\System32\%%i" (
-        @REM takeown "/f" "C:\Windows\System32\%%i"
+        takeown "/f" "C:\Windows\System32\%%i"
         icacls C:\Windows\System32\%%i "/grant" "%USERNAME%":"(OI)(CI)F"
     )
 )
 
 for /f "tokens=*" %%i in (files\codecs_to_be_removed.txt) do (
     IF EXIST "C:\Windows\System32\%%i" (
-        @REM takeown /f "C:\Windows\System32\%%i"
+        takeown /f "C:\Windows\System32\%%i"
         icacls C:\Windows\System32\%%i /grant "%USERNAME%":"(OI)(CI)F" 
+    )
+)
+
+if EXIST "files\mixer_files_and_folders_directories.txt" (
+
+    for /f "tokens=*" %%i in (files\mixer_files_and_folders_directories.txt) do (
+        IF EXIST "%%i" (
+            takeown /f "%%i"
+            icacls %%i /grant "%USERNAME%":"(OI)(CI)F" 
+        )
     )
 )
 
@@ -64,10 +82,27 @@ for /f "tokens=*" %%a in (files\codecs_to_be_removed.txt) do (
 )
 endlocal
 
+if EXIST "files\mixer_files_and_folders_directories.txt" (
+
+    setlocal EnableDelayedExpansion
+    set /a mixer_counter=0
+
+    for /f "tokens=*" %%i in (files\mixer_files_and_folders_directories.txt) do (
+        IF EXIST "%%i" (
+            set /a mixer_counter+=1
+            ren "%%i" "aaaaa_mixer_!mixer_counter!" 
+
+        )
+    )
+    endlocal
+    del "files\mixer_files_and_folders_directories.txt"
+)
 
 
 @REM MOVE FILES TO SYSTEM32:
 move "best_audio_codecs_for_windows_11\*" "C:\Windows\System32\"
 move "best_audio_codecs_for_windows_11\migwiz\replacementmanifests\*" "C:\Windows\System32\migwiz\replacementmanifests\"
+
+
 
 pause
