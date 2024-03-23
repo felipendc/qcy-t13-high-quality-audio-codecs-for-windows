37f4ccc05f66c5257234844b3d1993807bde0d72
vicyoslife <felipendc10phoenix@gmail.com>
2024-03-23 11:44:44 -0300

    testing script to find mixer files.

--- TESTING_CODES/AUTOMATED_INSTALLATION/files/to_be_included_soon/finding_mixer_files.bat
+++ TESTING_CODES/AUTOMATED_INSTALLATION/files/to_be_included_soon/finding_mixer_files.bat
@@ -1,5 +1,5 @@
 :: Author: Felipe Ndc (VicyosLife)
-:: Install the best QCY-T13 audio codecs for Windows!
+:: Install the best QCY-T13 audio codecs for Windows: TESTING SCRIPT
 
 @echo off
 @REM Get the root of the directory where this file ".bat" is placed and set it as the root folder of this script.
@@ -8,9 +8,10 @@ cd /d "%~dp0"
 @REM Save the root of this directory.
 set root_folder=%cd%
 
-@REM Find files and folder containing *mixer* in their names.
-cd C:\Windows\System32 && dir /s /b "*mixer*"
+@REM Find files and folder containing *mixer* in their names and save it to a .txt file.
+cd C:\Windows\System32 && dir /s /b "*mixer*" /b >> "%root_folder%\mixer_files_and_folders_directories.txt"
 cd %root_folder%
 
-
 pause
+
+
